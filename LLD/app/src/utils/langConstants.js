7299fb9cfb5b15eda44c8713a6e8a3ba9eff36f6
pank5214 <pankajkumar5214@gmail.com>
2024-02-23 00:13:32 +0530

    constants

--- LLD/app/src/utils/langConstants.js
+++ LLD/app/src/utils/langConstants.js
@@ -1,109 +1,74 @@
 export const LANG = {
   en: {
     title: "About Us",
-    titleDesc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo corporis
-    libero aut? Illum odit sint laudantium impedit asperiores dolores
-    deserunt laboriosam facere voluptatibus consequuntur animi doloremque
-    quia, quae pariatur odio aut optio vel. Fugit amet blanditiis
-    molestias voluptatibus magni earum. Minus odio delectus natus vero.
-    Dolor architecto sunt sint pariatur, vero, molestias vitae iure
-    quisquam eaque cumque beatae consectetur aliquid! Repellat dolorum
-    reiciendis error! Magnam, voluptatem illum porro sunt itaque nam vitae
-    harum eaque ducimus, sed cumque ratione consequuntur soluta nihil
-    culpa sit molestiae veritatis dicta dolor! Dignissimos facere eaque
-    iure neque? Architecto assumenda eveniet magnam culpa corporis labore
-    voluptas?`,
+    titleDesc: `The "About" section serves as a window into the essence and purpose of an entity, be it a person, organization, or project. It encapsulates the core values, history, and aspirations that define its existence. In approximately 150 words, an "About" paragraph should succinctly narrate the journey, philosophy, and goals. It might delve into the origins, milestones achieved, and the vision for the future. This section is a narrative bridge, connecting the audience with the identity and ethos of the subject. Whether detailing the mission of a company, the experiences that shaped an individual, or the objectives of a project, the "About" paragraph acts as a compelling introduction, leaving readers with a comprehensive understanding and a sense of connection. It is an opportunity to establish trust, convey authenticity, and invite further exploration into the fascinating narrative that lies beneath the surface.`,
     mission: "Mission",
     missionDesc: `Our mission is to provide a platform where users can share their
     experiences, connect with others, and discover new places to visit. We
     aim to make the world a smaller and more Our mission is to provide a
     platform where users can share their experiences, connect with others.`,
     product: "About the Product",
-    productDesc: `This product is a simple web application that allows users to create,
-    read, update and delete notes. The purpose of this project was to gain
-    experience in full-stack development by creating an application.`,
+    productDesc: `Introducing our innovative product, designed with precision and user-centric features to elevate your everyday experiences. Our product is a culmination of cutting-edge technology and thoughtful design, crafted to seamlessly integrate into your lifestyle. Whether you are a tech enthusiast, a professional, or someone seeking convenience, our product caters to your diverse needs.
+
+    With a sleek and ergonomic design, our product not only boasts aesthetic appeal but also prioritizes functionality. It incorporates advanced features that redefine industry standards, offering a user-friendly interface and unparalleled performance. From enhanced security protocols to intuitive controls, we have prioritized every detail to ensure a seamless and enjoyable user experience.
+    
+    What sets our product apart is its commitment to sustainability. We have embraced eco-friendly materials and energy-efficient components, aligning with our dedication to reducing environmental impact. Embrace the future with a product that not only meets but exceeds expectations, making a positive impact on your daily life. Welcome to a new era of innovation, welcome to a product designed with you in mind.`,
     address: "Address",
-    addressDesc: `2nd Floor, Kabra Excelsior, 6/A, 7th Main Rd, Koramangala 1A Block, 1st Block Koramangala, Koramangala, Bengaluru, Karnataka 560034`,
+    addressDesc: `
+    An address serves as a fundamental aspect of our daily lives, representing a precise location that enables communication, navigation, and identification. Comprising a combination of street names, building numbers, city, and often a postal code, an address facilitates the delivery of mail, packages, and essential services. Beyond its utilitarian function, an address holds cultural and social significance, anchoring individuals to specific communities or neighborhoods. It serves as a pivotal point for social interactions, helping friends, family, and colleagues locate one another. In the digital age, addresses extend to the virtual realm, with email addresses and IP addresses playing crucial roles in online communication and connectivity. As society evolves, addresses continue to play a pivotal role in organizing and structuring our interconnected world, bridging the physical and digital domains to ensure efficient communication, commerce, and the seamless functioning of our modern societies.`,
   },
 
   hi: {
     title: "हमारे बारे में",
-    titleDesc: `लोरेम इप्सम डोलर सिट अमेट कंसेक्टेचर एडिपिसिसिंग एलीट। Quo निगम
-    लिबरो ऑट? इल्लुम ओडिट सिंट लॉडेंटियम इम्पेडिट एस्परियोरेस डोलोरेस
-    डेसेरुंट लेबोरियोसम फेसेरे वोलुप्टैटिबस कॉन्सेक्वेंटूर एनिमी डोलोरेम्के
-    क्विया, क्यूए पैरियाटुर ओडियो ऑट ऑप्टियो वेल। फुगिट अमेट ब्लांडिटिस
-    मोलेस्टियास वॉलुप्टैटिबस मैग्नी ईयरम। माइनस ओडियो डेलेक्टस नेटस वेरो।
-    डोलोर आर्किटेक्टो संट सिंट पारियाटुर, वेरो, मोलेस्टियास विटे इयुरे
-    क्विस्क्वाम ईक कम्के बीटाए कंसेटेचर एलिक्विड! रेपेलेट डोलोरम
-    पुनः प्राप्त त्रुटि! मैग्नम, वोलुप्टेटेम इलम पोरो संट इटाके नाम विटे
-    हरम ईक डुसीमस, सेड कमक्यू रेशनन परिणामी सॉल्युटा निहिल
-    कल्पा सिट मोलेस्टिया वेरिटैटिस डिक्टा डोलर! डिग्निसिमोस फेसरे ईक
-    क्या आप चाहते हैं? आर्किटेक्टो असेंडेंडा इवनिएट मैग्नम कल्पा कॉर्पोरिस लेबोरे
-    voluptas?`,
+    titleDesc: `"अबाउट" अनुभाग किसी इकाई के सार और उद्देश्य में एक खिड़की के रूप में कार्य करता है, चाहे वह कोई व्यक्ति, संगठन या परियोजना हो। यह उन मूल मूल्यों, इतिहास और आकांक्षाओं को समाहित करता है जो इसके अस्तित्व को परिभाषित करते हैं। लगभग 150 शब्दों में, एक "अबाउट" पैराग्राफ में यात्रा, दर्शन और लक्ष्यों का संक्षेप में वर्णन होना चाहिए। यह मूल, हासिल किए गए मील के पत्थर और भविष्य के दृष्टिकोण में गहराई से उतर सकता है। यह खंड एक कथात्मक सेतु है, जो दर्शकों को विषय की पहचान और लोकाचार से जोड़ता है। चाहे किसी कंपनी के मिशन, किसी व्यक्ति को आकार देने वाले अनुभवों या किसी परियोजना के उद्देश्यों का विवरण हो, "अबाउट" पैराग्राफ एक सम्मोहक परिचय के रूप में कार्य करता है, जिससे पाठकों को व्यापक समझ और जुड़ाव की भावना मिलती है। यह विश्वास स्थापित करने, प्रामाणिकता व्यक्त करने और सतह के नीचे मौजूद आकर्षक कथा में आगे की खोज को आमंत्रित करने का एक अवसर है।`,
     mission: "उद्देश्य",
     missionDesc: `हमारा मिशन एक ऐसा मंच प्रदान करना है जहां उपयोगकर्ता अपनी बातें साझा कर सकें
     अनुभव प्राप्त करें, दूसरों से जुड़ें और घूमने के लिए नई जगहों की खोज करें। हम
     दुनिया को छोटा और बड़ा बनाने का लक्ष्य हमारा मिशन एक प्रदान करना है
     ऐसा मंच जहां उपयोगकर्ता अपने अनुभव साझा कर सकते हैं, दूसरों से जुड़ सकते हैं।`,
     product: "उत्पाद के बारे में",
-    productDesc: `यह उत्पाद एक सरल वेब एप्लिकेशन है जो उपयोगकर्ताओं को बनाने की अनुमति देता है,
-    नोट्स पढ़ें, अपडेट करें और हटाएं। इस परियोजना का उद्देश्य लाभ प्राप्त करना था
-    एक एप्लिकेशन बनाकर पूर्ण-स्टैक विकास में अनुभव।`,
+    productDesc: `पेश है हमारा नवोन्मेषी उत्पाद, जिसे आपके रोजमर्रा के अनुभवों को बेहतर बनाने के लिए सटीक और उपयोगकर्ता-केंद्रित सुविधाओं के साथ डिज़ाइन किया गया है। हमारा उत्पाद अत्याधुनिक तकनीक और विचारशील डिजाइन की पराकाष्ठा है, जिसे आपकी जीवनशैली में सहजता से एकीकृत करने के लिए तैयार किया गया है। चाहे आप तकनीकी उत्साही हों, पेशेवर हों, या सुविधा चाहने वाले व्यक्ति हों, हमारा उत्पाद आपकी विविध आवश्यकताओं को पूरा करता है।
+
+    एक चिकने और एर्गोनोमिक डिज़ाइन के साथ, हमारा उत्पाद न केवल सौंदर्यपूर्ण अपील का दावा करता है बल्कि कार्यक्षमता को भी प्राथमिकता देता है। इसमें उन्नत विशेषताएं शामिल हैं जो उद्योग मानकों को फिर से परिभाषित करती हैं, उपयोगकर्ता के अनुकूल इंटरफेस और अद्वितीय प्रदर्शन प्रदान करती हैं। उन्नत सुरक्षा प्रोटोकॉल से लेकर सहज ज्ञान युक्त नियंत्रण तक, हमने एक सहज और सुखद उपयोगकर्ता अनुभव सुनिश्चित करने के लिए हर विवरण को प्राथमिकता दी है।
+    
+    जो चीज हमारे उत्पाद को अलग करती है वह इसकी स्थिरता के प्रति प्रतिबद्धता है। हमने पर्यावरणीय प्रभाव को कम करने के प्रति अपने समर्पण के अनुरूप, पर्यावरण-अनुकूल सामग्रियों और ऊर्जा-कुशल घटकों को अपनाया है। भविष्य को ऐसे उत्पाद के साथ अपनाएं जो न केवल अपेक्षाओं को पूरा करता है बल्कि उससे भी बढ़कर है, जो आपके दैनिक जीवन पर सकारात्मक प्रभाव डालता है। नवाचार के एक नए युग में आपका स्वागत है, आपको ध्यान में रखकर डिज़ाइन किए गए उत्पाद में आपका स्वागत है।`,
     address: "पता",
-    addressDesc: `दूसरी मंजिल, काबरा एक्सेलसियर, 6/ए, 7वीं मुख्य रोड, कोरमंगला 1ए ब्लॉक, पहला ब्लॉक कोरमंगला, कोरमंगला, बेंगलुरु, कर्नाटक 560034`,
+    addressDesc: `एक पता हमारे दैनिक जीवन के मूलभूत पहलू के रूप में कार्य करता है, एक सटीक स्थान का प्रतिनिधित्व करता है जो संचार, नेविगेशन और पहचान को सक्षम बनाता है। सड़क के नाम, भवन संख्या, शहर और अक्सर एक पोस्टल कोड के संयोजन से, एक पता मेल, पैकेज और आवश्यक सेवाओं की डिलीवरी की सुविधा प्रदान करता है। अपने उपयोगितावादी कार्य से परे, एक संबोधन सांस्कृतिक और सामाजिक महत्व रखता है, जो व्यक्तियों को विशिष्ट समुदायों या पड़ोस से जोड़ता है। यह सामाजिक संपर्क के लिए एक महत्वपूर्ण बिंदु के रूप में कार्य करता है, मित्रों, परिवार और सहकर्मियों को एक-दूसरे का पता लगाने में मदद करता है। डिजिटल युग में, पते आभासी दायरे तक विस्तारित हो गए हैं, ईमेल पते और आईपी पते ऑनलाइन संचार और कनेक्टिविटी में महत्वपूर्ण भूमिका निभाते हैं। जैसे-जैसे समाज विकसित होता है, पते हमारी परस्पर जुड़ी दुनिया को व्यवस्थित और संरचित करने, कुशल संचार, वाणिज्य और हमारे आधुनिक समाजों की निर्बाध कार्यप्रणाली सुनिश्चित करने के लिए भौतिक और डिजिटल डोमेन को जोड़ने में महत्वपूर्ण भूमिका निभाते रहते हैं।`,
   },
 
   jp: {
     title: `私たちについて`,
-    titleDesc: `ロレム・イプサム・ドール・シット・アメットエリートのアディピシングを構築します。クォコーポリス
-    リベロオート？ イルム・オーディット・シント・ラウダンティウム・インディット・スペリオレス・ドロレス
-    絶望的な労働者と直面する運命の運命
-    クイア、クア・パリトゥール・オディオフギット・アメット・ブランディティス
-    モレスティアス・ヴォルプタティブス・マグニ・イヤーム。マイナスオディオデレクトゥスナトゥスベロ。
-    ドーロール建築家、サント・シント・パリトゥール、ベロ、モレスティア・ヴィタエ・イウレ
-    液体を結んでください!ドロラムを撃退する
-    ライシエンディスエラー!マグナム、ヴォルプテム・イルム・ポロ・サント・イタク・ナム・ヴィタエ
-    ハルム・エアク・ドゥシムス、セド・クムケ・ラシオネ・コンセクウントゥル・ソルタ・ニヒル
-    黙って座って、モレスティア・ベリタティス・ディクタ・ドール！ディグニッシモス フェイスレ エアク
-    うれネケ？建築家は、身体の労働力を最大限に高めることを想定しています
-    ヴォルプタス？`,
+    titleDesc: `「概要」セクションは、個人、組織、プロジェクトなど、エンティティの本質と目的を知るための窓として機能します。これには、その存在を定義する核となる価値観、歴史、願望が凝縮されています。 「概要」段落では、約 150 語で、これまでの道のり、哲学、目標を簡潔に説明する必要があります。起源、達成したマイルストーン、将来のビジョンを掘り下げる可能性があります。このセクションは物語の架け橋であり、聴衆を主題のアイデンティティや精神と結び付けます。企業の使命、個人を形作った経験、またはプロジェクトの目的を詳述する場合でも、「概要」段落は説得力のある導入部として機能し、読者に包括的な理解とつながりの感覚を与えます。それは信頼を確立し、信頼性を伝え、水面下にある魅力的な物語へのさらなる探究を促す機会です。`,
     mission: "ミッション",
     missionDesc: `私たちの使命は、ユーザーが自分の意見を共有できるプラットフォームを提供することです。
     体験し、他の人とつながり、新しい訪問先を発見してください。私たちは
     世界をより小さく、より小さくすることを目指します。 私たちの使命は、
     ユーザーが自分の経験を共有し、他のユーザーとつながることができるプラットフォーム。`,
     product: "製品について",
-    productDesc: `この製品は、ユーザーが作成できるシンプルな Web アプリケーションです。
-    メモの読み取り、更新、削除。このプロジェクトの目的は、
-    アプリケーションの作成によるフルスタック開発の経験。`,
+    productDesc: `日々の体験を向上させるために、正確でユーザー中心の機能を備えて設計された革新的な製品をご紹介します。当社の製品は最先端のテクノロジーと思慮深いデザインの集大成であり、あなたのライフスタイルにシームレスに統合できるように作られています。テクノロジー愛好家、専門家、利便性を求める人など、当社の製品はさまざまなニーズに応えます。
+
+    人間工学に基づいた洗練されたデザインの当社の製品は、見た目の美しさだけでなく、機能性も重視しています。業界標準を再定義する高度な機能が組み込まれており、ユーザーフレンドリーなインターフェイスと比類のないパフォーマンスを提供します。強化されたセキュリティ プロトコルから直感的なコントロールに至るまで、シームレスで楽しいユーザー エクスペリエンスを保証するために、あらゆる細部に優先順位を付けました。
+    
+    当社の製品を際立たせているのは、持続可能性への取り組みです。当社は、環境への影響を軽減するという取り組みに沿って、環境に優しい素材とエネルギー効率の高いコンポーネントを採用してきました。期待を満たすだけでなくそれを超え、日常生活にプラスの影響を与える製品で未来を受け入れましょう。イノベーションの新時代へようこそ。お客様のことを念頭に置いて設計された製品へようこそ。`,
     address: "住所",
-    addressDesc: `2階、カブラエクセルシオール、6/A、7th Main Rd、コラマンガラ1Aブロック、1ブロックコラマンガラ、コラマンガラ、バンガロール、カルナータカ州560034`,
+    addressDesc: `住所は私たちの日常生活の基本的な側面として機能し、通信、ナビゲーション、識別を可能にする正確な位置を表します。住所は、通りの名前、建物番号、市区町村、および多くの場合郵便番号の組み合わせで構成され、郵便物、荷物、および必要なサービスの配達を容易にします。住所はその実用的な機能を超えて、文化的および社会的重要性を持ち、個人を特定のコミュニティまたは近隣に固定します。これは社会的交流の極めて重要なポイントとして機能し、友人、家族、同僚がお互いを見つけるのに役立ちます。デジタル時代では、アドレスは仮想領域にまで拡張され、電子メール アドレスと IP アドレスがオンライン通信と接続において重要な役割を果たします。社会が進化するにつれて、アドレスは相互接続された世界を組織し構造化する上で重要な役割を果たし続け、物理的領域とデジタル領域を橋渡しして効率的なコミュニケーション、商取引、現代社会のシームレスな機能を確保します。`,
   },
 
   ru: {
     title: "О нас",
-    titleDesc: `Lorem ipsum dolor сидеть amet consectetur adipisicing elit. Quo corporis
-    либеро аут? Illum odit sint laudantium impedit asperiores dolores
-    Deserunt Laboriosam Facere Voluptatibus Consequuntur Animi Doloremque
-    quia, quae pariatur odio aut optio vel. Фугит амет бландитис
-    molestias voluptatibus magni Earum. Минус одио делектус натус веро.
-    Dolor Architecto sunt sint pariatur, vero, molestias vitae iure
-    quisquam eaque cumque beatae consectetur aliquid! Репеллат долорум
-    повторная ошибка! Magnam, voluptatem illum porro sunt itaque nam vitae
-    Harum eaque ducimus, sed cumqueratione consequuntur soluta nihil
-    culpa сидеть molestiae veritatis dicta dolor! Dignissimos facere eaque
-    я неке? Архитектор предполагает, что великая вина за труд
-    сладострастия?`,
+    titleDesc: `Раздел «О программе» служит окном в суть и предназначение сущности, будь то человек, организация или проект. Он воплощает в себе основные ценности, историю и стремления, которые определяют его существование. Параграф «О компании», состоящий примерно из 150 слов, должен кратко рассказать о путешествии, философии и целях. Он может углубиться в истоки, достигнутые вехи и видение будущего. Этот раздел представляет собой повествовательный мост, соединяющий аудиторию с личностью и духом субъекта. Независимо от того, подробно описывается миссия компании, опыт, который сформировал человека, или цели проекта, абзац «О программе» действует как убедительное введение, оставляя читателям всестороннее понимание и чувство связи. Это возможность установить доверие, передать подлинность и пригласить к дальнейшему исследованию увлекательного повествования, скрывающегося под поверхностью.`,
     mission: "Миссия",
     missionDesc: `Наша миссия — предоставить платформу, на которой пользователи могут делиться своими
     получать новые впечатления, общаться с другими людьми и открывать для себя новые места, которые стоит посетить. Мы
     стремимся сделать мир меньше и больше. Наша миссия — предоставить
     платформа, где пользователи могут делиться своим опытом, общаться с другими.`,
     product: "О продукте",
-    productDesc: `Этот продукт представляет собой простое веб-приложение, которое позволяет пользователям создавать,
-    читать, обновлять и удалять заметки. Целью этого проекта было получение
-    опыт full-stack разработки путем создания приложения.`,
+    productDesc: `Представляем наш инновационный продукт, разработанный с учетом точности и ориентированных на пользователя функций, которые сделают вашу повседневную жизнь лучше. Наш продукт — это кульминация передовых технологий и продуманного дизайна, созданный для того, чтобы легко интегрироваться в ваш образ жизни. Независимо от того, являетесь ли вы энтузиастом технологий, профессионалом или человеком, ищущим удобство, наш продукт удовлетворит ваши разнообразные потребности.
+
+    Наш продукт имеет элегантный и эргономичный дизайн и отличается не только эстетической привлекательностью, но и функциональностью. Он включает в себя расширенные функции, которые переопределяют отраслевые стандарты, предлагая удобный интерфейс и непревзойденную производительность. От улучшенных протоколов безопасности до интуитивно понятных элементов управления — мы уделяем приоритетное внимание каждой детали, чтобы обеспечить удобство и удобство работы пользователя.
+    
+    Что отличает наш продукт, так это его приверженность устойчивому развитию. Мы использовали экологически чистые материалы и энергоэффективные компоненты, что соответствует нашему стремлению снизить воздействие на окружающую среду. Откройте будущее с продуктом, который не только соответствует ожиданиям, но и превосходит их, оказывая положительное влияние на вашу повседневную жизнь. Добро пожаловать в новую эру инноваций, добро пожаловать в продукт, созданный специально для вас.`,
     address: "Адрес",
-    addressDesc: `2-й этаж, Кабра Эксельсиор, 6/A, 7-я главная улица, блок Корамангала 1А, 1-й блок Корамангала, Корамангала, Бангалор, Карнатака 560034`,
+    addressDesc: `Адрес служит фундаментальным аспектом нашей повседневной жизни, представляя точное местоположение, которое обеспечивает связь, навигацию и идентификацию. Адрес, состоящий из названий улиц, номеров домов, города и часто почтового индекса, облегчает доставку почты, посылок и основных услуг. Помимо своей утилитарной функции, адрес имеет культурное и социальное значение, привязывая людей к конкретным сообществам или районам. Он служит ключевой точкой социального взаимодействия, помогая друзьям, семье и коллегам найти друг друга. В эпоху цифровых технологий адреса распространяются на виртуальную реальность, при этом адреса электронной почты и IP-адреса играют решающую роль в онлайн-общении и подключении. По мере развития общества адреса продолжают играть ключевую роль в организации и структурировании нашего взаимосвязанного мира, соединяя физическую и цифровую сферы для обеспечения эффективной коммуникации, коммерции и бесперебойного функционирования наших современных обществ.`,
   },
 };
